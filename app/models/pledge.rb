2c3117bfd638284f7e5e8723c1c34d298d5879b7
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-21 15:19:22 -0400

    Add method validuser so project owner cannot submit pledge

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -3,4 +3,13 @@ class Pledge < ApplicationRecord
   belongs_to :project
 
   validates :dollar_amount, presence: true
+
+  validate :validuser
+
+  def validuser
+
+    if project.owner == user_id
+      return false
+
+  end
 end
