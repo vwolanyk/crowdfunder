fc9ff2941ba1c3b47512c1f030680962373218ee
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-25 19:05:18 -0400

    Update routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,13 @@ Rails.application.routes.draw do
     resources :projects, only: [:index, :new, :create, :show] do
     resources :pledges, only: [:create]
     resources :rewards, only: [:new, :create, :destroy]
+    resources :comments, only: [:new, :create, :destroy]
+    
   end
   resources :users, only: [:new, :create]
   resources :user_sessions, only: [:create]
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
