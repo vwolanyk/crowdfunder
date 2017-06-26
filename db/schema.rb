e23479acc325092ebde74471ca14ee5ebb568535
mikesmall <michael.l.a.small@gmail.com>
2017-06-26 16:06:25 -0400

    Category table migrations done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170626155133) do
-
+ActiveRecord::Schema.define(version: 20170626200438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_projects", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "project_id"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "comment_text"
