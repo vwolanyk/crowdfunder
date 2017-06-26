f4de8793722e15c35087a43fdd634bb43c514040
mikesmall <michael.l.a.small@gmail.com>
2017-06-25 20:23:52 -0400

    Added project association to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+  has_many :owned_projects, class_name: 'Project', foreign_key: 'owner_id'
+
   validates :password, length: { minimum: 8 }, on: :create
   validates :password, confirmation: true, on: :create
   validates :password_confirmation, presence: true, on: :create
 
   validates :email, uniqueness: true
 
-  
 end
