0ecd580cfcdb84d8488ef1b2716f5fbbb9b79e7a
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-25 21:38:22 -0400

    Update user association, generate Comment controller, Users can now leave comments on projects, owners can add updates to projects, different views for owners and user, cannot view a project if not logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+  has_many :comments
+  
   validates :password, length: { minimum: 8 }, on: :create
   validates :password, confirmation: true, on: :create
   validates :password_confirmation, presence: true, on: :create
 
   validates :email, uniqueness: true
 
-  
+
 end
