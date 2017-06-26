50f9312f4f712e630ad976c5e5ca7e67c627511e
mikesmall <michael.l.a.small@gmail.com>
2017-06-25 21:04:36 -0400

    Added pledge association to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
   has_many :owned_projects, class_name: 'Project', foreign_key: 'owner_id'
 
+  has_many :pledged_projects, class_name: 'Pledge', foreign_key: 'user_id'
+
   validates :password, length: { minimum: 8 }, on: :create
   validates :password, confirmation: true, on: :create
   validates :password_confirmation, presence: true, on: :create
